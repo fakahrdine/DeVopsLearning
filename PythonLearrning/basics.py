eacd6d29a757eb834746ffe41942111e9a7f2f27
FakharDine <fakharodine2015@gmail.com>
2026-03-11 20:47:41 -0400

    I addde new py file to leaarn paython basics

---
+++ PythonLearrning/basics.py
@@ -0,0 +1,36 @@
+from operator import truediv
+from xmlrpc.client import boolean
+
+print("hello world")
+print("hello world")
+print("hello world")
+print('Mohamed ' * 20)
+# storing data in computer memory using variable price = 100
+#ints
+price = 2000
+print(price)
+# flaots
+grade = 17,5
+print(grade)
+#boolean
+is_coldout = False
+print(is_coldout)
+patient_name = 'John Smith'
+age =20
+patient_status = 'new'
+is_new = True
+
+
+name =input('what is your name?')
+print('Hi'+name+'!')
+ages =input('what is your age?')
+print('oh !!' +ages+'you still yong !!')
+
+
+
+
+
+
+
+
+
